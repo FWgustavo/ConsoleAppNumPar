571ee6fcfdb34fde5974fd250a9a72c2b72b0389
Usuario <Usuario@CPD02-M01>
2024-03-26 10:32:00 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppNumPar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNumPar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+            while (cont <= 100) 
+            {
+                Console.WriteLine(cont);
+                cont += 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
